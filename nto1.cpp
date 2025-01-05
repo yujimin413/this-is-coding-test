2b546544e313b4c3cfc9471b8e34ed4039cab777
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2025-01-05 12:31:56 +0900

    nto1

---
+++ nto1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, K, cnt = 0;
+    scanf("%d %d", &N, &K);
+    // printf("%d %d\n", N, K);
+
+    while (N != 1) {
+        if (N % K == 0) {
+            N /= K;
+            cnt++;
+        }
+        else {
+            N -= 1;
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+}
